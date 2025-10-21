d8bd068e0c9f1970fffe64eceaa58c93121ca2ae
Anurag996 <anuragppraja2408@gmail.com>
2025-10-21 14:04:43 +0530

    Add files via upload

---
+++ OBJECTIVE 1/MYLIB.H FILE.cpp
@@ -0,0 +1,12 @@
+//  HEADER FILE--
+
+#ifndef MYLIB_H
+#define MYLIB_H
+
+int reverseDigits(int n);
+int isPrime(int num);
+int isArmstrong(int num);
+int isAdams(int num);
+int isPrimePalindrome(int num);
+
+#endif
